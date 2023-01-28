7531dabcefe2fa8b2c5ba79efe7f3056ee81f06b
Giap Tran <txgvnn@gmail.com>
2023-01-28 13:10:38 +0700

    guxti/packages: tree-sitter-grammar* pin to commit

--- guxti/packages/emacs.scm
+++ guxti/packages/emacs.scm
@@ -93,86 +93,86 @@
 
 (define-public emacs-tree-sitter-grammar-bash
   (emacs-tree-sitter-grammar
-   "tree-sitter" "bash" "0.19.0" "v0.19.0"
+   "tree-sitter" "bash" "0.19.0" "b6667be"
    "18c030bb65r50i6z37iy7jb9z9i8i36y7b08dbc9bchdifqsijs5"
    "Bash grammar for tree-sitter in Emacs"))
 
 (define-public emacs-tree-sitter-grammar-c
   (emacs-tree-sitter-grammar
-   "tree-sitter" "c" "0.20.2" "v0.20.2"
+   "tree-sitter" "c" "0.20.2" "5aa0bbb"
    "1w03r4l773ki4iq2xxsc2pqxf3pjsbybq3xq4glmnsihgylibn8v"
    "C grammar for tree-sitter in Emacs"))
 
 (define-public emacs-tree-sitter-grammar-cpp
   (emacs-tree-sitter-grammar
-   "tree-sitter" "cpp" "0.20.0" "v0.20.0"
+   "tree-sitter" "cpp" "0.20.0" "05cf203"
    "0hxcpdvyyig8njga1mxp4qcnbbnr1d0aiy27vahijwbh98b081nr"
    "C++ grammar for tree-sitter in Emacs"))
 
 (define-public emacs-tree-sitter-grammar-css
   (emacs-tree-sitter-grammar
-   "tree-sitter" "css" "0.19.0" "v0.19.0"
+   "tree-sitter" "css" "0.19.0" "9668e88"
    "014jrlgi7zfza9g38hsr4vlbi8964i5p7iglaih6qmzaiml7bja2"
    "CSS grammar for tree-sitter in Emacs"))
 
 (define-public emacs-tree-sitter-grammar-c-sharp
   (emacs-tree-sitter-grammar
-   "tree-sitter" "c-sharp" "0.20.0" "v0.20.0"
+   "tree-sitter" "c-sharp" "0.20.0" "70fd2cb"
    "0lijbi5q49g50ji00p2lb45rvd76h07sif3xjl9b31yyxwillr6l"
    "C# grammar for tree-sitter in Emacs"))
 
 (define-public emacs-tree-sitter-grammar-elixir
   (emacs-tree-sitter-grammar
-   "elixir-lang" "elixir" "0.0.1"
+   "elixir-lang" "elixir" "b11cec3"
    "0ba537df8692179f34cccc7efed05de6cf5178aa"
    "1k35nfwnz8zjiw2mg8d6nphxp3gb7yhb0a5vw4cz2h3n10yyq75i"
    "Elixir grammar for tree-sitter in Emacs"))
 
 (define-public emacs-tree-sitter-grammar-go
   (emacs-tree-sitter-grammar
-   "tree-sitter" "go" "0.19.1" "rust-0.19.1"
+   "tree-sitter" "go" "0.19.1" "e41dd56"
    "0nxs47vd2fc2fr0qlxq496y852rwg39flhg334s7dlyq7d3lcx4x"
    "Go grammar for tree-sitter in Emacs"))
 
 (define-public emacs-tree-sitter-grammar-html
   (emacs-tree-sitter-grammar
-   "tree-sitter" "html" "0.19.0" "v0.19.0"
+   "tree-sitter" "html" "0.19.0" "d93af48"
    "1hg7vbcy7bir6b8x11v0a4x0glvqnsqc3i2ixiarbxmycbgl3axy"
    "HTML grammar for tree-sitter in Emacs"))
 
 (define-public emacs-tree-sitter-grammar-java
   (emacs-tree-sitter-grammar
-   "tree-sitter" "java" "0.20.0" "v0.20.0"
+   "tree-sitter" "java" "0.20.0" "ac14b4b"
    "1i9zfgqibinz3rkx6yws1wk49iys32x901dki65qihbxcmcfh341"
    "Java grammar for tree-sitter in Emacs"))
 
 (define-public emacs-tree-sitter-grammar-javascript
   (emacs-tree-sitter-grammar
-   "tree-sitter" "javascript" "0.20.0" "rust-0.20.0"
+   "tree-sitter" "javascript" "0.20.0" "fdeb68a"
    "175yrk382n2di0c2xn4gpv8y4n83x1lg4hqn04vabf0yqynlkq67"
    "JavaScript grammar for tree-sitter in Emacs"))
 
 (define-public emacs-tree-sitter-grammar-json
   (emacs-tree-sitter-grammar
-   "tree-sitter" "json" "0.19.0" "v0.19.0"
+   "tree-sitter" "json" "0.19.0" "8960792"
    "06pjh31bv9ja9hlnykk257a6zh8bsxg2fqa54al7qk1r4n9ksnff"
    "JSON grammar for tree-sitter in Emacs"))
 
 (define-public emacs-tree-sitter-grammar-python
   (emacs-tree-sitter-grammar
-   "tree-sitter" "python" "0.20.0" "v0.20.0"
+   "tree-sitter" "python" "0.20.0" "2b9e9e0"
    "14nnnblbjxyri8x21kj59agiy3cn4fwfrab3dmidykdyq2r46f5w"
    "Python grammar for tree-sitter in Emacs"))
 
 (define-public emacs-tree-sitter-grammar-rust
   (emacs-tree-sitter-grammar
-   "tree-sitter" "rust" "0.20.3" "v0.20.3"
+   "tree-sitter" "rust" "0.20.3" "0431a2c"
    "149jhy01mqvavwa8jlxb8bnn7sxpfq2x1w35si6zn60b7kqjlx8f"
    "Rust grammar for tree-sitter in Emacs"))
 
 (define-public emacs-tree-sitter-grammar-typescript
   (emacs-tree-sitter-grammar
-   "tree-sitter" "typescript" "0.20.1" "v0.20.1"
+   "tree-sitter" "typescript" "0.20.1" "5d20856"
    "07fl9d968lal0aqj4f0n16p3n94cjkgfp54wynfr8gbdkjss5v5x"
    "TypeScript/TSX grammar for tree-sitter in Emacs"))
 
