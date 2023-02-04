dae9c8974dc1254d553fb149d1195af4ef81d52c
Giap Tran <txgvnn@gmail.com>
2023-02-04 17:32:58 +0700

    guxti/packages: add emacs-next-nox-tree-sitter

--- guxti/packages/emacs.scm
+++ guxti/packages/emacs.scm
@@ -3,6 +3,7 @@
   #:use-module (guix gexp)
   #:use-module (guix download)
   #:use-module (guix git-download)
+  #:use-module (guix build-system)
   #:use-module (guix build-system gnu)
   #:use-module (guix utils)
   #:use-module ((guix licenses) #:prefix license:)
@@ -10,6 +11,13 @@
   #:use-module (gnu packages text-editors)
   #:use-module (gnu packages texinfo)
   #:use-module (gnu packages emacs))
+
+(define (%emacs-modules build-system)
+  (let ((which (build-system-name build-system)))
+    `((guix build ,(symbol-append which '-build-system))
+      (guix build utils)
+      (srfi srfi-1)
+      (ice-9 ftw))))
 
 ;; https://lists.gnu.org/archive/html/guix-devel/2022-12/msg00073.html
 (define (emacs-tree-sitter-grammar org lang version commit hash
@@ -231,3 +239,23 @@
        (modify-inputs
            (package-inputs emacs-next-pgtk)
          (prepend tree-sitter))))))
+
+(define-public emacs-next-nox-tree-sitter
+  (package/inherit emacs-next-tree-sitter
+    (name "emacs-next-nox-tree-sitter")
+    (build-system gnu-build-system)
+    (inputs (modify-inputs (package-inputs emacs-next-tree-sitter)
+              (delete "libx11" "gtk+" "libxft" "libtiff" "giflib" "libjpeg"
+                      "imagemagick" "libpng" "librsvg" "libxpm" "libice"
+                      "libsm" "cairo" "pango" "harfbuzz"
+                      ;; These depend on libx11, so remove them as well.
+                      "libotf" "m17n-lib" "dbus")))
+    (arguments
+     (substitute-keyword-arguments (package-arguments emacs-next-tree-sitter)
+       ((#:configure-flags flags #~'())
+        #~(delete "--with-cairo" #$flags))
+       ((#:modules _) (%emacs-modules build-system))
+       ((#:phases phases)
+        #~(modify-phases #$phases
+            (delete 'restore-emacs-pdmp)
+            (delete 'strip-double-wrap)))))))
