0991dd41a3fecd27e4d7711b830002d00debcd5c
Giap Tran <txgvnn@gmail.com>
2023-07-15 03:51:00 +0000

    Update Guix channel
    - Update consult, corfu follow Guix

--- guxti/packages/emacs-xyz.scm
+++ guxti/packages/emacs-xyz.scm
@@ -138,10 +138,10 @@ installed packages.")
       (license license:gpl3+))))
 
 (define-public emacs-consult-me
-  (let ((commit "0.34"))
+  (let ((commit "0.35"))
     (package
       (name "emacs-consult")
-      (version "0.34.20230427")
+      (version "0.35.20230715")
       (source
        (origin
          (method git-fetch)
@@ -149,21 +149,30 @@ installed packages.")
                (url "https://github.com/minad/consult")
                (commit commit)))
          (sha256
-          (base32 "1ggbvc5ylsw430w05fjl4vk1hmim45mwah7cyr94g03rwjhng1sc"))
-         (file-name (git-file-name name version))
-         ))
+          (base32 "0a20rfqv2yfwqal1vx6zzg92qgr32p3rp7n6awnyb010jnykqszw"))
+         (file-name (git-file-name name version))))
       (build-system emacs-build-system)
-      (propagated-inputs (list emacs-compat))
       (arguments
-       `(#:phases
-         (modify-phases %standard-phases
-           (add-after
-               'unpack 'fix-version
-             (lambda _
-               (substitute*
-                   (string-append (string-drop ,name (string-length "emacs-")) ".el")
-                 (("^;; Version: ([^/[:blank:]\r\n]*)(.*)$")
-                  (string-append ";; Version: " ,version "\n"))))))))
+       (list
+        #:phases
+        #~(modify-phases %standard-phases
+            (add-after 'unpack 'fix-version
+              (lambda _
+                (substitute*
+                    (string-append (string-drop #$name (string-length "emacs-")) ".el")
+                  (("^;; Version: ([^/[:blank:]\r\n]*)(.*)$")
+                   (string-append ";; Version: " #$version "\n")))))
+            (add-after 'install 'makeinfo
+              (lambda _
+                (invoke "emacs"
+                        "--batch"
+                        "--eval=(require 'ox-texinfo)"
+                        "--eval=(find-file \"README.org\")"
+                        "--eval=(org-texinfo-export-to-info)")
+                (install-file "consult.info"
+                              (string-append #$output "/share/info")))))))
+      (native-inputs (list texinfo))
+      (propagated-inputs (list emacs-compat))
       (home-page "https://github.com/minad/consult")
       (synopsis "Consulting completing-read")
       (description "This package provides various handy commands based on the
@@ -446,4 +455,51 @@ deadlines that are due in your agenda.  To perform a one-shot check call
 (org-alert-deadlines).  To enable repeated checking call (org-alert-enable) and
 to disable call (org-alert-disable).  You can set the checking interval by
 changing the org-alert-interval variable to the number of seconds you'd like.")
+    (license license:gpl3+)))
+
+(define-public emacs-corfu-terminal-next
+  (package
+    (name "emacs-corfu-terminal")
+    (version "0.6")
+    (source
+     (origin
+       (method git-fetch)
+       (uri
+        (git-reference
+         (url "https://codeberg.org/akib/emacs-corfu-terminal")
+         (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0f9zd4q9w5bkli7nbpdkmhglhvafisglhhqb7wfvghpp9gbcafkp"))))
+    (build-system emacs-build-system)
+    (propagated-inputs (list emacs-corfu emacs-popon-next))
+    (home-page "https://codeberg.org/akib/emacs-corfu-terminal/")
+    (synopsis "Replace corfu child frames with popups")
+    (description
+     "This package replaces the child frames @code{emacs-corfu} uses
+with popups, which also work in the terminal.")
+    (license license:gpl3+)))
+
+(define-public emacs-popon-next
+  (package
+    (name "emacs-popon")
+    (version "0.13")
+    (source
+     (origin
+       (method git-fetch)
+       (uri
+        (git-reference
+         (url "https://codeberg.org/akib/emacs-popon")
+         (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "046l8is3rg0c6qhiy7wh91pcdhwqhnw47md8q231w8mxnw5b1n5j"))))
+    (build-system emacs-build-system)
+    (home-page "https://codeberg.org/akib/emacs-popon/")
+    (synopsis "Pop floating text on a window")
+    (description
+     "@code{emacs-popon} allows you to pop text on a window, what we call
+a popon.  Popons are window-local and sticky, they don't move while
+scrolling, and they even don't go away when switching buffer, but you
+can bind a popon to a specific buffer to only show on that buffer.")
     (license license:gpl3+)))
